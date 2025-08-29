22bc6f53c2c0052bb8edbed763c1704ea2ee8831
sannan <sannan.yousaf@dubizzlelabs.com>
2025-08-29 18:38:18 +0500

    using pgvector

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import getpass
 import os
 from pydantic import Field, BaseModel
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_core.documents import Document
 
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
@@ -137,12 +140,12 @@ class StrucutredResponse(BaseModel):
 
 structured_llm = llm.with_structured_output(StrucutredResponse)
 chain_one = (
-    {
-        "article": lambda x: x['article']
-    }
-    | first_prompt
-    | llm
-    | {"article_title": lambda x: x.content}
+        {
+            "article": lambda x: x['article']
+        }
+        | first_prompt
+        | llm
+        | {"article_title": lambda x: x.content}
 )
 
 chain_two = (
@@ -159,3 +162,69 @@ print(title)
 
 response = chain_two.invoke({"article": article})
 print(response.title)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+connection = "postgresql+psycopg://sannan:@localhost:6024/langchain"
+
+collection_name = "my_docs"
+
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+
+docs = [
+    Document(
+        page_content="there are cats in the pond",
+        metadata={"id": 1, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="ducks are also found in the pond",
+        metadata={"id": 2, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="fresh apples are available at the market",
+        metadata={"id": 3, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the market also sells fresh oranges",
+        metadata={"id": 4, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the new art exhibit is fascinating",
+        metadata={"id": 5, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a sculpture exhibit is also at the museum",
+        metadata={"id": 6, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a new coffee shop opened on Main Street",
+        metadata={"id": 7, "location": "Main Street", "topic": "food"},
+    ),
+    Document(
+        page_content="the book club meets at the library",
+        metadata={"id": 8, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="the library hosts a weekly story time for kids",
+        metadata={"id": 9, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="a cooking class for beginners is offered at the community center",
+        metadata={"id": 10, "location": "community center", "topic": "classes"},
+    ),
+]
+
+vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+
+# delete from vector store
+vector_store.delete(ids=["3"])
+
+results = vector_store.similarity_search_with_score(
+    "kitty", k=10
+)
+for doc in results:
+    print(doc)
